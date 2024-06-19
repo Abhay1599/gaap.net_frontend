a9393c9d5079e3e7528e5bb8bdc14f65af13580b
Prarabdha Sharma <kinchu98sharma@gmail.com>
2024-06-19 16:25:23 -0400

    about and contact

--- gaap_frontend/src/app/app.routes.ts
+++ gaap_frontend/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './dashboard/home/home.component';
+import { AboutComponent } from './dashboard/about/about.component';
+import { ContactComponent } from './dashboard/contact/contact.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent } 
+  //{ path: '', component: HomeComponent } 
+   //{ path: '', component: AboutComponent } 
+   { path: '', component: ContactComponent } 
 ];
