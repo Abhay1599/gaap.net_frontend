ab503320488ccad749f4752a66ce04dbfb469cbd
ABHAY <abhaychadha15@gmail.co>
2024-06-19 13:28:06 -0400

    home

--- gaap_frontend/src/app/layout/header/header.component.ts
+++ gaap_frontend/src/app/layout/header/header.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
+  constructor(private primengConfig: PrimeNGConfig) {}
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
 }
