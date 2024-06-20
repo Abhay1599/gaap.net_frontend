2a20a8244cd137b75e7e4c5f535566b99549a124
ABHAY <abhaychadha15@gmail.co>
2024-06-20 00:58:21 -0400

    chnages in header and routes

--- gaap_frontend/src/app/app.routes.ts
+++ gaap_frontend/src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './dashboard/home/home.component';
 import { AboutComponent } from './dashboard/about/about.component';
 import { ContactComponent } from './dashboard/contact/contact.component';
+import { BusinessTransitionComponent } from './components/GResources/business-transition/business-transition.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'dashboard/home', component: HomeComponent },
-  { path: 'dashboard/about', component: AboutComponent },
-  { path: 'dashboard/contact', component: ContactComponent },
+  { path: 'company/about', component: AboutComponent },
+  { path: 'company/contact', component: ContactComponent },
+  { path: 'resources/business-transition', component: BusinessTransitionComponent },
+
 ];
